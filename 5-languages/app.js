7bc32a917e949e8977d120de5a7cd253a90c67fc
Aleksandr <aleksandr.pankov.87@list.ru>
2025-03-04 21:59:08 +0300

    ДЗ 6.9 с изменениями

--- 5-languages/app.js
+++ 5-languages/app.js
@@ -1,20 +1,17 @@
 "use strict"
 
-const language = prompt();
+const language = prompt().toLowerCase();
 
 console.log(language);
 
 switch (language) {
-	case 'en':
-	case 'EN':	
+	case 'en':	
 		console.log('Hello!');
 		break;
 	case 'ru':
-	case 'RU':
 		console.log('Здравствуйте!');
 		break;
 	case 'de':
-	case 'DE':
 		console.log('Guten Tag!');
 		break;
 	default:
