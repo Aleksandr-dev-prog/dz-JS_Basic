7a3a76e9a94cce481196856ca0d295aaa1328b96
Aleksandr <aleksandr.pankov.87@list.ru>
2025-03-24 22:02:01 +0300

    ДЗ 9.11 изм.

--- 7-converter/app.js
+++ 7-converter/app.js
@@ -1,79 +1,219 @@
 "use strict"
 
-function convert(sum, initialCurrency) {
-
-    let convertCurrency;
 // 1 способ
 
-    /*
-    if (initialCurrency === 'RUB'){
-       convertCurrency = sum/90;//USD
-       console.log(`${convertCurrency} USD`);
-    } else if (initialCurrency === 'USD'){
-        convertCurrency = sum * 90;//RUB   
-        console.log(`${convertCurrency} RUB`); 
-    } else {
-        console.log(null);
+function converter(sum, currency, targetCurrency) {
+
+    const USD = 90;
+    const EUR = 100;
+    
+    currency = currency.toUpperCase();
+    targetCurrency = targetCurrency.toUpperCase();
+
+    switch (currency) {
+        case 'USD':
+            switch (targetCurrency) {
+                case 'RUB':
+                    return sum * USD;
+                case 'EUR':
+                    return (sum * USD) / EUR;
+                default:
+                    return null;
+            }
+        case 'EUR':
+            switch (targetCurrency) {
+                case 'RUB':
+                    return sum * EUR;
+                case 'USD':
+                    return (sum * EUR) / USD;
+                default:
+                    return null;
+            }
+        case 'RUB':
+            switch (targetCurrency) {
+                case 'USD':
+                    return sum / USD;
+                case 'EUR':
+                    return sum / EUR;
+                default:
+                    return null;
+            }
+        default:
+            return null;
     }
-    */
+}
+
+console.log(converter(100,'USD','RUB'));
 
 // 2 способ
 
-    switch (initialCurrency) {
-        case 'RUB':
-            convertCurrency = sum/90;//USD
-            console.log(`${convertCurrency} USD`);
-            break;
+function convertSum(sum, from, to) {
+    return ((sum * from) / to).toFixed(2);
+}
+
+function getTargetCurrencyIcon(targetCurrency) {
+        switch (targetCurrency) {
+            case 'RUB':
+                return 'руб';
+            case 'EUR':
+                return '€';
+            case 'USD':
+                return '$';
+            default: return '';
+        }
+}
+
+function messageTemplate(value, icon){//шаблон сообщения
+    return `${value} ${icon}`;
+}
+
+function converter1(sum, currency, targetCurrency) {
+    const ALLOW_CURRENCY_LIST = ['RUB', 'EUR', 'USD'];//разрешённый список валют
+    const USD = 91.33;
+    const RUB = 1;
+    const EUR = 112.33;
+
+    currency = currency.toUpperCase();//переводим в верхний регистр
+    targetCurrency = targetCurrency.toUpperCase();//переводим в верхний регистр
+    const icon = getTargetCurrencyIcon(targetCurrency);//добавляем иконку к валюте
+
+    if (currency === targetCurrency) {
+        return messageTemplate(sum, icon);//если конвертируемая валюта равна целевой валюте, то возвращаем сумму и иконку валюты
+    }
+    if (!ALLOW_CURRENCY_LIST.includes(currency)) {//если валюта отсутствует в списке валют
+        console.log('Входящая валюта мне неизвестна');
+        return null;
+    }
+    if (!ALLOW_CURRENCY_LIST.includes(targetCurrency)) {
+        console.log('Исходящая валюта мне неизвестна');
+        return null;
+    }
+    
+    let value = null;
+
+    switch (currency) {
         case 'USD':
-            convertCurrency = sum * 90;//RUB
-            console.log(`${convertCurrency} RUB`);
-            break;
-        default:
-            console.log(null);
-            break;
+            switch (targetCurrency) {
+                case 'RUB': value = convertSum(sum, USD, RUB); break;
+                case 'EUR': value = convertSum(sum, USD, EUR); break;
+            }; break;
+        case 'RUB':
+            switch (targetCurrency) {
+                case 'USD': value = convertSum(sum, RUB, USD); break;
+                case 'EUR': value = convertSum(sum, RUB, EUR); break;
+            }; break;
+        case 'EUR':
+            switch (targetCurrency) {
+                case 'RUB': value = convertSum(sum, EUR, RUB); break;
+                case 'USD': value = convertSum(sum, EUR, USD); break;
+            }; break;
     }
+
+    return messageTemplate(value, icon)
 }
 
-convert (9000, 'RUB');// 100 USD
-
-// 3 спопсоб
-
-let RUB = 1;
-let CNY = 12;
-let USD = 90;
+console.log(converter1(1000,'Rub','uSd'));
 
 
-let EUR = 100;
 
-// функция написана с учетом незнания массивов и объектов)))
-// как реализовать конструкцию else if через switch case, я не знаю.
+// 3 способ
 
-function convert2(sum,initialCurrency,convertCurrency){
-    if (initialCurrency == RUB && convertCurrency == USD) {
-        console.log(`${sum/USD} USD`);
-    } else if (initialCurrency == RUB && convertCurrency == EUR){
-        console.log(`${sum/EUR} EUR`); 
-    } else if (initialCurrency == USD && convertCurrency == RUB){
-        console.log(`${sum * USD} RUB`); 
-    } else if (initialCurrency == EUR && convertCurrency == RUB){
-        console.log(`${sum * EUR} RUB`); 
-    } else if (initialCurrency == USD && convertCurrency == EUR){
-        console.log(`${sum * (USD/EUR)} EUR`); 
-    } else if (initialCurrency == EUR && convertCurrency == USD){
-        console.log(`${sum * (EUR/USD)} USD`); 
-    } else {
-        console.log(null);
-    }
+function convertSum1(sum, from, to) {
+    return ((sum * from) / to).toFixed(2);
 }
 
-convert2 (900, RUB, USD);// 10 USD
-convert2 (900, RUB, EUR);// 9 EUR
-convert2 (900, USD, RUB);// 81000 RUB
-convert2 (900, EUR, RUB);// 90000 RUB
-convert2 (900, USD, EUR);// 810 EUR
-convert2 (900, EUR, USD);// 1000 USD
-convert2 (900, RUB, CNY);// null
+function getTargetCurrencyIcon1(targetCurrency) {
+        switch (targetCurrency) {
+            case 'RUB':
+                return 'руб';
+            case 'EUR':
+                return '€';
+            case 'USD':
+                return '$';
+            default: return '';
+        }
+}
 
+function messageTemplate1(value, icon){
+    return `${value} ${icon}`;
+}
+
+function converter2(sum, currency, targetCurrency) {
+    const ALLOW_CURRENCY_LIST = [['RUB', 1], ['EUR', 112.33], ['USD', 91.33]];
+    const flatList = ALLOW_CURRENCY_LIST.flat();
+    currency = currency.toUpperCase();
+    targetCurrency = targetCurrency.toUpperCase();
+
+    if (!flatList.includes(currency)) {
+        console.log('Входящая валюта мне неизвестна');
+        return null;
+    }
+    if (!flatList.includes(targetCurrency)) {
+        console.log('Исходящая валюта мне неизвестна');
+        return null;
+    }
+    const icon = getTargetCurrencyIcon1(targetCurrency);
+    if (currency === targetCurrency) {
+        return messageTemplate1(sum, icon);
+    }
+
+    const currentIndex = flatList.indexOf(currency) / 2;
+    console.log(currentIndex);//0
+    const targetIndex = flatList.indexOf(targetCurrency) / 2;
+    console.log(targetIndex);//1
+    const [, currentValue] = ALLOW_CURRENCY_LIST[currentIndex];
+    const [, targetValue] = ALLOW_CURRENCY_LIST[targetIndex];
+    const result = convertSum1(sum, currentValue, targetValue)
+    return messageTemplate1(result, icon);
+}
+
+console.log(converter2(1000,'Rub','eUr'));
+
+
+
+// 4 способ
+
+const CURRENCY_LIST = {
+    USD: { value: 90, marker: '$'},
+    EUR: { value: 100, marker: '€' },
+    RUB: { value: 1, marker: '₽' },
+}
+
+function isValidCurrency(currency){
+    const isExists = Object.keys(CURRENCY_LIST).includes(currency);
+    if(!isExists){
+        console.log(`Валюта [${currency}] не поддерживается для обмена`);
+    }
+    return isExists;
+}
+
+function converter3(value, inCurrency, outCurrency) {
+    inCurrency = inCurrency.toUpperCase();
+    outCurrency = outCurrency.toUpperCase();
+
+    if (inCurrency === outCurrency) {
+        return `Входящая и исходящая валюта одинаковы. Конвертация невозможна`;
+    }
+
+    // Проверяем наличие входящей валюты для обмена
+    if (!isValidCurrency(inCurrency)) {
+        return null;
+    }
+    // Проверяем наличие исходящей валюты для обмена
+    if (!isValidCurrency(outCurrency)) {
+        return null;
+    }
+
+
+    const { value: a, marker: im } = CURRENCY_LIST[inCurrency]; // im = inMarker
+    const { value: b, marker: om } = CURRENCY_LIST[outCurrency]; // im = outMarker
+
+    return `В результате обмена ${value}${im} Вы получите ${(value * a / b).toFixed(2)}${om}`
+}
+
+console.log(converter3(1500, 'usd', 'usd'));
+console.log(converter3(1500, 'rub', 'usd'));
+console.log(converter3(1500, 'eur', 'usd'));
 
 
 
--- 8-crypto/app.js
+++ 8-crypto/app.js
@@ -1,28 +1,101 @@
 "use strict"
 
-let password = 'password';
-let wrong = 'wrong';
-
-function crypto (a) {
-    let code = a.slice (0,4).split('').reverse().join('');
-    let code1 = a.slice (4,8).split('').reverse().join('');
-    let code2 = code1.slice (1,3).split('').reverse().join('');
-    let str = code + 'd' + code2 + 'w';
-    return str;
-}
-let cryptoFunc = crypto(password);
-console.log(cryptoFunc);//ssapdorw
-
-function check (a,b){
-    let code = a.slice (0,4).split('').reverse().join('');
-    let code1 = a.slice (4,8).split('').reverse().join('');
-    let code2 = code1.slice (1,3).split('').reverse().join('');
-    let str = code + 'w' + code2 + 'd';
-    return (str === b);
+function crypto (password) {
+    let arr = password.split('');
+    let averageIndex = Math.ceil(arr.length/2);//средний индекс
+    let firstArr = arr.slice (0,averageIndex).reverse();
+    let secondArr = arr.splice (averageIndex,arr.length - 1).reverse();
+    [firstArr[0],firstArr[firstArr.length - 1],secondArr[0],secondArr[secondArr.length - 1]] = [secondArr[secondArr.length - 1],secondArr[0],firstArr[firstArr.length - 1],firstArr[0]];
+    let newArr = firstArr.concat(secondArr);
+    let res = newArr.join('');
+    return res;
 }
 
-console.log(check (cryptoFunc,password));//true
-console.log(check (cryptoFunc,wrong));//false
+function check(encryptedPassword, originalPassword) {
+    if (!encryptedPassword || !originalPassword) {
+        return false;
+    }
+    return originalPassword === crypto(encryptedPassword);
+}
+
+let str = prompt().toLowerCase();
+if (str.length < 4){
+    console.log('Пароль должен быть не менее 4 символов');
+} else {
+    let encryptedPassword = crypto (str);
+    let decrypted = crypto (encryptedPassword);
+    console.log(str,encryptedPassword,decrypted, str === decrypted);
+    console.log(check(encryptedPassword,str));
+}
+
+
+// Генерация пароля
+
+const CHARS = `!#$%&0123456789?@ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvw`
+function crypt(password) {
+    if (!password || !password.length) {
+        return NaN;
+    }
+    const splitedArray = password.split('');
+    if (splitedArray.length < 8) {
+        return false;
+    }
+
+    const middleIndex = Math.floor(splitedArray.length / 2);
+    const firstHalf = splitedArray.slice(0, middleIndex).reverse();
+    const secondHalf = splitedArray.slice(middleIndex).reverse();
+    const length = secondHalf.length;
+    
+    [firstHalf[1], secondHalf[length - 2]] = [secondHalf[length - 2], firstHalf[1]];
+    [secondHalf[1], firstHalf[length - 2]] = [firstHalf[length - 2], secondHalf[1]];
+
+    [firstHalf[0], secondHalf[length - 1]] = [secondHalf[length - 1], firstHalf[0]];
+    [secondHalf[0], firstHalf[length - 1]] = [firstHalf[length - 1], secondHalf[0]];
+
+    const encryptedPassword = firstHalf.concat(secondHalf);
+    return encryptedPassword.join('');
+}
+
+function checkPassword(encryptedPassword, originalPassword) {
+    if (!encryptedPassword || !originalPassword) {
+        return false;
+    }
+    return originalPassword === crypt(encryptedPassword);
+}
+
+function getRandomNumber(min = 0, max = 86) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandomPassword(length = 12) {
+    // const symbolArray = Array.from({ length: 87 }, (_, index) => String.fromCharCode(index + 33));
+    const password = Array.from({ length }, () => CHARS[getRandomNumber(0, CHARS.length)]);
+    return password.join('');
+}
+
+/*
+    logging and test function with console
+*/
+
+const password = getRandomPassword();
+const encrypted = crypt(password);
+const decrypted = crypt(encrypted)
+
+console.log('Пароль:', password);
+console.log('Зашифрованный пароль:', encrypted);
+console.log('Проверка пароля (обратимость):', decrypted === password);
+console.log('Проверка пароля (check):', checkPassword(encrypted, password));
+
+
+
+
+
+
+
+
+
+
+
 
 
 
